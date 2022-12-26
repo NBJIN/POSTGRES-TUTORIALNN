6f0dcb51f94b6917bf868d42fcd0eaa03fdcec36
noreen <noreennelligan1@gmail.com>
2022-12-26 22:23:30 +0000

    install libraries

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -17,16 +17,19 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistID" = %s', [51])
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
 
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-# cursor.execute('SELECT * FROM "Album" WHERE "ArtistID" = %s', [51])
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', [Queen])
+# Query 6 - select all tracks where the composer is "Queen" from the "Track"
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query 7 - 
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["R.E.M"])
 
 # fetch the results (multiple)
-results = cursor.fetchall()
+# results = cursor.fetchall()
 
 # fetch the result (single)
 results = cursor.fetchone()
